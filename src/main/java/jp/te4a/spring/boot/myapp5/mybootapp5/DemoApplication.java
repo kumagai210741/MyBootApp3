94ade73154848d4634ef6df5c8d19b16afbb3826
Kumagai00 <metarou6@gmail.com>
2024-09-06 17:17:28 +0900

    modified:   pom.xml 	deleted:    src/main/java/com/example/demo/MyBootApp3.java 	renamed:    src/main/java/com/example/demo/DemoApplication.java -> src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/DemoApplication.java 	new file:   src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java 	modified:   src/main/resources/application.yml 	new file:   src/main/webapp/WEB-INF/views/index.jsp 	modified:   src/test/java/com/example/demo/DemoApplicationTests.java

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/DemoApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package jp.te4a.spring.boot.myapp5.mybootapp5;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
  
     public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
+ 
 }
